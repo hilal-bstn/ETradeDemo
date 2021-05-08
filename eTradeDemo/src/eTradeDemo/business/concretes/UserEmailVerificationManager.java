e63cb2820b91a45ceea9027f3de38e985a26196e
Hilal Baştan <hilalbastan@gmail.com>
2021-05-08 13:51:52 +0300

    Update UserEmailVerificationManager.java

--- eTradeDemo/src/eTradeDemo/business/concretes/UserEmailVerificationManager.java
+++ eTradeDemo/src/eTradeDemo/business/concretes/UserEmailVerificationManager.java
@@ -7,7 +7,7 @@ public class UserEmailVerificationManager implements UserVerifyService {
 	@Override
 	public void Send() 
 	{
-		System.out.println("Kullanıcı doğrulama emaili gönderildi.");
+		System.out.println("KullanÃ½cÃ½ doÃ°rulama maili gÃ¶nderildi.");
 	}
 
 }
